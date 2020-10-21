840beb7584130b6f6ad3be435333043eae742382
priyansiva <priyansiva1995@gmail.com>
2020-10-21 13:40:41 +0530

    initial commit

---
+++ src/data/cats.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    name: 'Fish',
+    breed: 'tuxedo',
+    gender: 'male',
+    age: 20,
+    color: 'black/white',
+    weigth: 13,
+    location: 'fourside',
+    notes: 'Sweet kitty'
+  },
+  {
+    name: 'Henry',
+    breed: 'tabby',
+    gender: 'male',
+    age: 20,
+    color: 'gray',
+    weigth: 17,
+    location: 'thread',
+    notes: 'Super friendly'
+  },
+  {
+    name: 'Roger',
+    breed: 'tabby',
+    gender: 'male',
+    age: 20,
+    color: 'orange/white',
+    weigth: 37,
+    location: 'thread',
+    notes: 'Super friendly'
+  }
+]
---
+++ src/data/dogs.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    name: 'Sheeba',
+    breed: 'Coolie',
+    gender: 'female',
+    age: 7,
+    color: 'black/white',
+    weigth: 34,
+    location: 'fourside',
+    notes: 'Pure breed, Trained for competitons'
+  },
+  {
+    name: 'Zeus',
+    breed: 'afgan hound',
+    gender: 'male',
+    age: 9,
+    color: 'gray',
+    weigth: 68,
+    location: 'thread',
+    notes: 'Super friendly'
+  },
+  {
+    name: 'Hillary',
+    breed: 'nul',
+    gender: 'female',
+    age: 17,
+    color: 'orange/white',
+    weigth: 37,
+    location: 'thread',
+    notes: 'Super friendly'
+  }
+]
